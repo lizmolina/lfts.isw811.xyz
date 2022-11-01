b0c5541b73bb5a2d1253faa8f9e87da44b5d5dce
Lizmolina <lizmolinadmt@gmail.com>
2022-11-01 00:51:08 -0600

    Build the Log In Page

--- routes/web.php
+++ routes/web.php
@@ -26,6 +26,9 @@ Route::get('register', [RegisterController::class, 'create'])->middleware('guest
 
 Route::post('register', [RegisterController::class, 'store'])->middleware('guest');
 
+Route::get('login', [SessionsController::class, 'create'])->middleware('guest');
+Route::post('login', [SessionsController::class, 'store'])->middleware('guest');
+
 Route::post('logout', [SessionsController::class, 'destroy'])->middleware('auth');
 
 
